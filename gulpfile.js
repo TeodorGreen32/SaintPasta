c59a05bbff7684a34986bd2b855f05501a0660b2
TeodorGreen32 <teodorgreen@yandex.ru>
2022-10-21 21:11:08 +0500

    Main-Gulp-Fix

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ const babel = require('gulp-babel');
 const uglify = require('gulp-uglify-es').default;
 const notify = require('gulp-notify');
 const sourceMaps = require('gulp-sourcemaps');
-
+const image = require('gulp-image');
 
 const clean = () => {
     return del('dist');
